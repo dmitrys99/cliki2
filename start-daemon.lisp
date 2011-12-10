582cbe8cfe13b4d730047462db95d3c2ceabeb92
Vladimir Sedach <vsedach@gmail.com>
2011-12-10 14:22:38 -0500

    Daemonize script now correctly sets fasl output folder after dropping priveleges

--- start-daemon.lisp
+++ start-daemon.lisp
@@ -2,7 +2,7 @@
 
 ;; start sbcl as sbcl --sysinit /home/cliki2/.sbclrc
 
-(cl-daemonize:daemonize)
+(cl-daemonize:daemonize) ;; this has to be first for some reason
 
 (swank:create-server :port 4010 :dont-close t :coding-system "utf-8-unix")
 
@@ -12,6 +12,10 @@
 ;; drop priveleges
 (sb-posix:setgid (sb-posix:group-gid (sb-posix:getgrnam "cliki2")))
 (sb-posix:setuid (sb-posix:passwd-uid (sb-posix:getpwnam "cliki2")))
+
+;; fix up fasl locations for ASDF
+(sb-posix:putenv "HOME=/home/cliki2")
+(asdf:clear-output-translations)
 
 ;; *must* be done as cliki2 user or store gets wiped
 (load-cliki-store)
