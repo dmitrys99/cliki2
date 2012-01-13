b8cac3ec8c5f36760fa57dea5bfa6ed50f3a8233
Vladimir Sedach <vsedach@gmail.com>
2012-01-13 13:36:39 -0500

    Made random string be used in cookie authentication, fixed possible race condition

--- src/authentication.lisp
+++ src/authentication.lisp
@@ -2,27 +2,35 @@
 (in-readtable cliki2)
 
 (defvar *session-secrets* (make-hash-table :test 'equal))
+(defvar *session-secrets-lock* (bt:make-lock))
 
 (defun logout ()
-  (remhash (cookie-in "cliki2auth") *session-secrets*)
-  (set-cookie "cliki2auth" :value "" :path "/"))
+  (bt:with-lock-held (*session-secrets-lock*)
+    (remhash (cookie-in "cliki2auth") *session-secrets*))
+  (set-cookie "cliki2auth" :value "" :path "/")
+  nil)
 
-(defun hash^2-account-password (account)
+(defun hash^2-account-password (salt account)
   (ironclad:byte-array-to-hex-string
    (ironclad:digest-sequence
     'ironclad:sha256
-    (babel:string-to-octets (account-password-digest account)))))
+    (babel:string-to-octets
+     (concatenate 'string (account-password-digest account) salt)))))
 
 (defun login (account)
-  (let ((secret #?"${(make-random-string 20)}.${(hash^2-account-password account)}"))
-    (setf (gethash secret *session-secrets*) account)
+  (let* ((salt (make-random-string 20))
+         (secret #?"${salt}.${(hash^2-account-password salt account)}"))
+    (bt:with-lock-held (*session-secrets-lock*)
+      (setf (gethash secret *session-secrets*) account))
     (set-cookie "cliki2auth" :value secret :path "/")))
 
 (defun account-auth ()
-  (let* ((cookie (cookie-in "cliki2auth")))
-    (awhen (gethash cookie *session-secrets*)
-      (if (string= (subseq cookie (1+ (position #\. cookie)))
-                   (hash^2-account-password it))
+  (let* ((cookie (cookie-in "cliki2auth"))
+         (dot (position #\. cookie)))
+    (awhen (bt:with-lock-held (*session-secrets-lock*)
+             (gethash cookie *session-secrets*))
+      (if (string= (subseq cookie (1+ dot))
+                   (hash^2-account-password (subseq cookie 0 dot) it))
           it
           (logout)))))
 
